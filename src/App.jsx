fe1fad739b48b48f1928a8c9dde111828b2ddfc9
Parbat Nil Bera <parbatnilbera743@gmail.com>
2025-03-27 10:10:29 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-gray-900 min-w-screen min-h-screen text-white text-6xl font-bold">
+      The site is currently under development
+    </div>
+  );
+};
+
+export default App;
